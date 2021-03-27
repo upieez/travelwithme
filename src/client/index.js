f369869a777e94a5140a5942d2df6e50a8898f69
Samuel Huang <samuelhuang@Samuels-MacBook-Pro.local>
2021-03-28 00:06:57 +0800

    feat: successful api calls

---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+import "./js/app";
+import { handleSubmit } from "./js/formHandler";
+
+import "./styles/styles.scss";
+
+export { handleSubmit };
---
+++ src/client/js/formHandler.js
@@ -0,0 +1,65 @@
+async function handleSubmit(event) {
+  event.preventDefault();
+  console.log("Hey you clicked me!");
+
+  const countryName = document.getElementById("country-search").value;
+
+  try {
+    const geoNamesValues = await fetchGeoNamesAPI(countryName);
+
+    const latitude = geoNamesValues.geonames[0].lat;
+    const longitude = geoNamesValues.geonames[0].lng;
+
+    const weather = await fetchWeatherbitAPI(latitude, longitude);
+    const picture = await fetchPixabayAPI(countryName);
+
+    document.querySelector(".search-result").innerHTML = `
+    <div>
+    <h2> You have chosen ${countryName} </h2>
+    <img src="${picture.hits[0].webformatURL}">
+    <span> The weather is currently ${weather.data[0].temp} in ${countryName}! </span>
+    <div>`;
+
+    console.log("weather", weather);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
+
+async function fetchGeoNamesAPI(country) {
+  const getGeoNames = await fetch("/api/geonames", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ country }),
+  });
+  return await getGeoNames.json();
+}
+
+async function fetchWeatherbitAPI(latitude, longitude) {
+  const getWeatherbit = await fetch("/api/weatherbit", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ latitude, longitude }),
+  });
+  return await getWeatherbit.json();
+}
+
+async function fetchPixabayAPI(country) {
+  const getWeatherbit = await fetch("/api/pixabay", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ country }),
+  });
+  return await getWeatherbit.json();
+}
+
+export { handleSubmit };
