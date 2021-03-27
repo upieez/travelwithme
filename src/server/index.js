a5e0d9f6cb2a7e4270d2b4db496fa3d442879c96
Samuel Huang <samuelhww@gmail.com>
2021-03-28 00:49:18 +0800

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 dotenv.config();
 
