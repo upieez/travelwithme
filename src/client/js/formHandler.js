3cf8c475dddef64393b9f2102aa8acc977cbefb1
Samuel Huang <samuelhww@gmail.com>
2021-03-28 16:13:06 +0800

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,8 +1,14 @@
 async function handleSubmit(event) {
   event.preventDefault();
-  console.log("Hey you clicked me!");
 
   const countryName = document.getElementById("country-search").value;
+  const searchBox = document.getElementById("country-search");
+  const dateBox = document.getElementById("departure");
+  const errorMsg = document.querySelector(".error-message");
+
+  document.querySelector(
+    ".search-result"
+  ).innerHTML = ` <h2> You have chosen ${countryName} </h2> <p>Loading...</p>`;
 
   try {
     const geoNamesValues = await fetchGeoNamesAPI(countryName);
@@ -14,19 +20,30 @@ async function handleSubmit(event) {
     const picture = await fetchPixabayAPI(countryName);
     const flag = await fetchRESTCountriesAPI(countryName);
 
-    document.querySelector(".search-result").innerHTML = `
-    <div>
-    <h2> You have chosen ${countryName} </h2>
-    <img src="${picture.hits[0].webformatURL}" />
-    <img src="${flag}" style="height:54px;width:96px;" />
-    <p> The weather is currently ${weather.data[0].temp} in ${countryName}! </p>
-    <img src="icons/${weather.data[0].weather.icon}.png" alt="">
-    <p> The weather is currently ${weather.data[0].weather.icon} in ${countryName}! </p>
-    </div>`;
+    const renderCard = Client.renderCard(countryName, weather, picture, flag);
 
-    console.log("weather", weather);
+    document.querySelector(".search-result").innerHTML = `${renderCard}`;
+
+    const data = {
+      countryName: countryName,
+      weather: weather.data[0],
+      picture: picture.hits[0].webformatURL,
+      flag: flag,
+    };
+
+    await saveAllData(data);
+    searchBox.style.borderColor = "green";
   } catch (error) {
-    console.log("Error: ", error);
+    searchBox.style.borderColor = "red";
+    errorMsg.textContent = "Not Found";
+    errorMsg.style.color = "red";
+    document.querySelector(
+      ".search-result"
+    ).innerHTML = ` <h2> You have chosen ${countryName} </h2> <p> Sorry this destination is currently unavailable :( </p>`;
+    console.error("Error: ", error);
+  } finally {
+    searchBox.value = "";
+    dateBox.value = "";
   }
 }
 
@@ -70,8 +87,22 @@ async function fetchRESTCountriesAPI(country) {
   const getCountryFlag = await (
     await fetch(`https://restcountries.eu/rest/v2/name/${country}`)
   ).json();
-  console.log("getCountryFlag", getCountryFlag);
   return getCountryFlag[0].flag;
+}
+
+async function saveAllData(data) {
+  console.log("hmmm");
+  const saveData = await fetch("/save-trip", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ data }),
+  });
+  const hey = await saveData.json();
+  console.log("123", hey);
+  return hey;
 }
 
 export { handleSubmit };
